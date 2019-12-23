d5b499aac487978d97a499b859d9cf78d911e2e0
Laszlo Nagy <nagylzs@gmail.com>
2019-12-23 13:07:54 +0100

    LEDControl.set_channel and LEDControl.set_duty

--- ports/esp32/esp32_ledcontrol.c
+++ ports/esp32/esp32_ledcontrol.c
@@ -37,11 +37,14 @@
 // Default PWM resolution
 #define LEDCONTROL_DEFAULT_RES (LEDC_TIMER_10_BIT)
 
-typedef struct _esp32_ledcontrol_obj_t {
+typedef struct _esp32_ledcontrol_obj_t
+{
     mp_obj_base_t base;
-    u_int timer_num;
+    ledc_timer_t timer_num;
     u_int freq_hz;
     u_int duty_res;
+    u_int duties[LEDC_CHANNEL_MAX];
+    gpio_num_t pins[LEDC_CHANNEL_MAX];
 } _esp32_ledcontrol_obj_t;
 
 const mp_obj_type_t esp32_ledcontrol_type;
@@ -49,8 +52,10 @@ const mp_obj_type_t esp32_ledcontrol_type;
 // Defined in machine_time.c; simply added the error message
 // Fixme: Should use this updated error hadline more widely in the ESP32 port.
 //        At least update the method in machine_time.c.
-STATIC esp_err_t check_esp_err(esp_err_t code) {
-    if (code) {
+STATIC esp_err_t check_esp_err(esp_err_t code)
+{
+    if (code)
+    {
         mp_raise_msg(&mp_type_OSError, esp_err_to_name(code));
     }
 
@@ -60,26 +65,28 @@ STATIC esp_err_t check_esp_err(esp_err_t code) {
 // Number of LEDControl objects created
 STATIC u_int instances = 0;
 
-STATIC mp_obj_t esp32_ledcontrol_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
+STATIC mp_obj_t esp32_ledcontrol_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args)
+{
 
     static const mp_arg_t allowed_args[] = {
-        { MP_QSTR_timer_num,       MP_ARG_REQUIRED |                  MP_ARG_INT, {.u_int = LEDC_TIMER_0} },
-        { MP_QSTR_freq_hz,         MP_ARG_REQUIRED | MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = LEDCONTROL_DEFAULT_FREQ} },
-        { MP_QSTR_duty_res,        MP_ARG_KW_ONLY  | MP_ARG_INT, {.u_int = LEDCONTROL_DEFAULT_RES} },
+        {MP_QSTR_timer_num, MP_ARG_REQUIRED | MP_ARG_INT, {.u_int = LEDC_TIMER_0}},
+        {MP_QSTR_freq_hz, MP_ARG_REQUIRED | MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = LEDCONTROL_DEFAULT_FREQ}},
+        {MP_QSTR_duty_res, MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = LEDCONTROL_DEFAULT_RES}},
     };
     mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all_kw_array(n_args, n_kw, all_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
 
     ledc_timer_config_t ledc_timer = {
-        .duty_resolution = args[2].u_int,     // resolution of PWM duty
-        .freq_hz = args[1].u_int,             // frequency of PWM signal
-        .speed_mode = LEDC_HIGH_SPEED_MODE,   // timer mode
-        .timer_num = args[0].u_int,           // timer index
+        .duty_resolution = args[2].u_int,   // resolution of PWM duty
+        .freq_hz = args[1].u_int,           // frequency of PWM signal
+        .speed_mode = LEDC_HIGH_SPEED_MODE, // timer mode
+        .timer_num = args[0].u_int,         // timer index
     };
     check_esp_err(ledc_timer_config(&ledc_timer));
-    
-    if (instances==0) {
-        check_esp_err(ledc_fade_func_install(0  /* (ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED */));
+
+    if (instances == 0)
+    {
+        check_esp_err(ledc_fade_func_install(0 /* (ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_SHARED */));
     }
     instances += 1;
 
@@ -88,67 +95,122 @@ STATIC mp_obj_t esp32_ledcontrol_make_new(const mp_obj_type_t *type, size_t n_ar
     self->timer_num = ledc_timer.timer_num;
     self->freq_hz = ledc_timer.freq_hz;
     self->duty_res = ledc_timer.duty_resolution;
+    for (int i = 0; i < LEDC_CHANNEL_MAX; i++)
+    {
+        self->duties[i] = -1;
+        self->pins[i] = -1;
+    }
 
     return MP_OBJ_FROM_PTR(self);
 }
 
-
-STATIC void esp32_ledconrol_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
+STATIC void esp32_ledcontrol_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind)
+{
     _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    mp_printf(print, "RMT(timer=%u, freq_hz=%u, duty_res=%u)",
-            self->timer_num, self->freq_hz, self->duty_res);
+    mp_printf(print, "LEDControl(timer=%u, freq_hz=%u, duty_res=%u)",
+              self->timer_num, self->freq_hz, self->duty_res);
 }
 
-
-STATIC mp_obj_t esp32_ledcontrol_deinit(mp_obj_t self_in) {
-    _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
+STATIC mp_obj_t esp32_ledcontrol_deinit(mp_obj_t self_in)
+{
+    //_esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
     instances -= 1;
-    if (instances==0) {
+    if (instances == 0)
+    {
         ledc_fade_func_uninstall();
     }
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_ledcontrol_deinit_obj, esp32_ledcontrol_deinit);
 
+STATIC mp_obj_t esp32_ledcontrol_set_channel(mp_obj_t self_in, mp_obj_t channel_obj, mp_obj_t pin)
+{
+    _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    ledc_channel_t channel = mp_obj_get_int(channel_obj);
+    if (channel < 0 || channel > LEDC_CHANNEL_MAX)
+    {
+        mp_raise_ValueError("invalid channel");
+    }
+    gpio_num_t pin_num = machine_pin_get_id(pin);
+    self->pins[channel] = pin_num;
+    self->duties[channel] = 0;
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num = pin_num,                /*!< the LEDC output gpio_num, if you want to use gpio16, gpio_num = 16 */
+        .speed_mode = LEDC_HIGH_SPEED_MODE, /*!< LEDC speed speed_mode, high-speed mode or low-speed mode */
+        .channel = channel,                 /*!< LEDC channel (0 - 7) */
+        .intr_type = LEDC_INTR_DISABLE,     /*!< configure interrupt, Fade interrupt enable  or Fade interrupt disable */
+        .timer_sel = self->timer_num,       /*!< Select the timer source of channel (0 - 3) */
+        .duty = 0,                          /*!< LEDC channel duty, the range of duty setting is [0, (2**duty_resolution)] */
+        .hpoint = 0
+    };
+    check_esp_err(ledc_channel_config(&ledc_channel));
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(esp32_ledcontrol_set_channel_obj, esp32_ledcontrol_set_channel);
+
+STATIC mp_obj_t esp32_ledcontrol_set_duty(mp_obj_t self_in, mp_obj_t channel_obj, mp_obj_t duty_obj) {
+    _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    ledc_channel_t channel = mp_obj_get_int(channel_obj);
+    if (channel < 0 || channel > LEDC_CHANNEL_MAX)
+    {
+        mp_raise_ValueError("invalid channel");
+    }
+    u_int duty = mp_obj_get_int(duty_obj);
+    gpio_num_t pin_num = self->pins[channel];
+    if (pin_num == -1) {
+        mp_raise_ValueError("unconfigured channel");
+    }
+    check_esp_err(ledc_set_duty(LEDC_HIGH_SPEED_MODE, channel, duty));
+    check_esp_err(ledc_update_duty(LEDC_HIGH_SPEED_MODE, channel));
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(esp32_ledcontrol_set_duty_obj, esp32_ledcontrol_set_duty);
 
 
 // Return the timer number
-STATIC mp_obj_t esp32_ledcontrol_timer_num(mp_obj_t self_in) {
+STATIC mp_obj_t esp32_ledcontrol_timer_num(mp_obj_t self_in)
+{
     _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
     return mp_obj_new_int(self->timer_num);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_ledcontrol_timer_num_obj, esp32_ledcontrol_timer_num);
 
 // Return the frequency
-STATIC mp_obj_t esp32_ledcontrol_freq_hz(mp_obj_t self_in) {
+STATIC mp_obj_t esp32_ledcontrol_freq_hz(mp_obj_t self_in)
+{
     _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
     return mp_obj_new_int(self->freq_hz);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_ledcontrol_freq_hz_obj, esp32_ledcontrol_freq_hz);
 
 // Return the duty resolution
-STATIC mp_obj_t esp32_ledcontrol_duty_res(mp_obj_t self_in) {
+STATIC mp_obj_t esp32_ledcontrol_duty_res(mp_obj_t self_in)
+{
     _esp32_ledcontrol_obj_t *self = MP_OBJ_TO_PTR(self_in);
     return mp_obj_new_int(self->duty_res);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_ledcontrol_duty_res_obj, esp32_ledcontrol_duty_res);
 
 STATIC const mp_rom_map_elem_t esp32_ledcontrol_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&esp32_ledcontrol_deinit_obj) },
-    { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&esp32_ledcontrol_deinit_obj) },
+    {MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&esp32_ledcontrol_deinit_obj)},
+    {MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&esp32_ledcontrol_deinit_obj)},
 
-    { MP_ROM_QSTR(MP_QSTR_timer_num), MP_ROM_PTR(&esp32_ledcontrol_timer_num_obj) },
-    { MP_ROM_QSTR(MP_QSTR_freq_hz), MP_ROM_PTR(&esp32_ledcontrol_freq_hz_obj) },
-    { MP_ROM_QSTR(MP_QSTR_duty_res), MP_ROM_PTR(&esp32_ledcontrol_duty_res_obj) },
+    {MP_ROM_QSTR(MP_QSTR_timer_num), MP_ROM_PTR(&esp32_ledcontrol_timer_num_obj)},
+    {MP_ROM_QSTR(MP_QSTR_freq_hz), MP_ROM_PTR(&esp32_ledcontrol_freq_hz_obj)},
+    {MP_ROM_QSTR(MP_QSTR_duty_res), MP_ROM_PTR(&esp32_ledcontrol_duty_res_obj)},
+
+    {MP_ROM_QSTR(MP_QSTR_set_channel), MP_ROM_PTR(&esp32_ledcontrol_set_channel_obj)},
+    {MP_ROM_QSTR(MP_QSTR_set_duty), MP_ROM_PTR(&esp32_ledcontrol_set_duty_obj)},
 
 };
 
 STATIC MP_DEFINE_CONST_DICT(esp32_ledcontrol_locals_dict, esp32_ledcontrol_locals_dict_table);
 
 const mp_obj_type_t esp32_ledcontrol_type = {
-    { &mp_type_type },
+    {&mp_type_type},
     .name = MP_QSTR_LEDControl,
     .make_new = esp32_ledcontrol_make_new,
-    .print = esp32_ledconrol_print,
+    .print = esp32_ledcontrol_print,
     .locals_dict = (mp_obj_t)&esp32_ledcontrol_locals_dict,
 };
